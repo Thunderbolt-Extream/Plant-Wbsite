0a8a099f504ba0e51cca467b22c39d4fb8367ab1
czzyPranto <czzypranto@gmail.com>
2025-01-23 06:33:59 +0600

    page finished

--- main.js
+++ main.js
@@ -16,6 +16,29 @@ link.addEventListener("click",() => {
     })
     /*==================== CHANGE BACKGROUND HEADER ====================*/
     /*==================== SHOW SCROLL UP ====================*/
+const scrollUp = () =>{
+  const scrollUpBtn = document.getElementById('scroll-up')
+  if (this.scrollY >= 250){
+   scrollUpBtn.classList.remove('-bottom-1/2')
+   scrollUpBtn.classList.add('bottom-4')
+}
+else {
+    scrollUpBtn.classList.add('-bottom-1/2')
+    scrollUpBtn.classList.remove('bottom-4')}
+}
+    window.addEventListener('scroll', scrollUp)
+
+    /*==================== CHANGE BACKGROUND HEADER====================*/
+    const scrollHeader = () =>{
+      const header = document.getElementById('navbar')
+      if (this.scrollY >= 50){
+       header.classList.add('border-b','border-yellow-300')
+    }
+    else {
+        header.classList.remove('border-b','border-yellow-500')}
+    }
+        window.addEventListener('scroll',scrollHeader)
+
     /*==================== SWIPER====================*/
     const swiper = new Swiper('.swiper', {
         // Optional parameters
@@ -50,4 +73,39 @@ link.addEventListener("click",() => {
       });
       
     /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+const activeLink= () =>{
+  const sections = document.querySelectorAll('section')
+  const navLink = document.querySelectorAll('.nav-link')
+  let current ="home"
+
+  sections.forEach(section =>{
+    const sectionTop = section.offsetHeight;
+    if(this.scrolly >= sectionTop -60){
+      current = section.getAttribute('id')
+    }})
+    navLink.forEach(item =>{
+      item.classList.remove('active')
+      if(item.href.includes(current)){
+        item.classList.add('active')
+      }
+    })
+  }
+window.addEventListener('scroll', activeLink)
+
     /*==================== SCROLL REVEAL ANIMATION ====================*/
+    const sr = ScrollReveal({
+      origin: 'top',
+      distance: '60px',
+      duration: 2500,
+      delay: 300,
+      reset: true,
+    })
+
+    sr.reveal('.home__data,.about__top,.popular__top,.review__top,.review__swiper,.footer__icon,.footer__content ,copy__right')
+    sr.reveal('.home__image',{delay:500,scale:0.5})
+    sr. reveal('.service__card,.popular__card',{interval:100})
+    sr.reveal('.about__leaf',{delay:1000,origin:'right'})
+    sr.reveal('.about__item__1-content,.about__item__2-img',{origin:'left',interval:100})
+    sr.reveal('.about__item__2-content,.about__item__1-img',{origin:'right',interval:100})
+    sr.reveal('.review__leaf,.footer__floral',{delay:1000,origin:'left'})
